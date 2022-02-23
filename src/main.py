05674589f8245f4c0f1b21082670ab4d78fa8303
eyad shhadeh <eyad_shhadeh@live.com>
2022-02-22 18:02:11 -0800

    remove 'f'

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ target_mesh = mesh.Mesh.from_file('assets/raw_039.stl')
 
 #rotate
 target_mesh.rotate([0, 1, 0], math.radians(180))
-mesh.Mesh.save(target_mesh,filename=f'output/raw_039_rotated.stl')
+mesh.Mesh.save(target_mesh,filename='output/raw_039_rotated.stl')
 
 #add the vectors to the plot
 axes.add_collection3d(mplot3d.art3d.Poly3DCollection(target_mesh.vectors))
